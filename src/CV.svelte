eb656d9e0d918fdbd84906bf8b08c9d36780ca06
jankiel7410 <michal7410@gmail.com>
2020-11-01 14:59:34 +0100

    extract experience into separate components

--- src/CV.svelte
+++ src/CV.svelte
@@ -1,9 +1,12 @@
+<script lang="ts">
+    import ExpEntry from './ExpEntry.svelte'
+</script>
+
 <main class='container mx-auto mt-4'>
     <header>
         <h1 class='text-6xl font-hairline leading-none'>Michał Cymbalista</h1>
         <h2 class='text-4xl'>Full-Stack Developer</h2>
     </header>
-    <div class='strip bg-teal-600 my-2 opacity-50'></div>
     <aside>
         <header class="text-2xl text-gray-700 border-solid border-b-2 border-teal-600">
             <h3>Profile Summary</h3>
@@ -25,15 +28,72 @@
             <h4 class='text-xl text-gray-800 font-bold'>
                 Full-Stack Engineer
             </h4>
-            <p class=''>Tooploox is a software house working mainly with clients based in US.</p>
+            <p class='mt-1 text-gray-700 italic'>Tooploox is a software house working mainly with clients based in US.</p>
             <p>I was involved in the following projects:</p>
-            <ul class='mt-1 ml-2 text-base'>
-                <li class='' role="listitem">
-                    <p>Web application for displaying and transforming macroeconomic time series.</p>
-                    <p>Responsible for the frontend;
-                        including setting up the infrastructure, CI/CD, choice of the technology, etc.</p>
-                    <p>Main challenges: making React and d3 play together nicely, displaying thousands and thousands of data points as an SVG.</p>
-                </li>
+            <ul class='mt-2 ml-4 text-sm divide-y divide-solid divide-gray-400'>
+                <ExpEntry>
+                    <p slot="desc">
+                        Web application for displaying and transforming macroeconomic time series.<br>
+                        Frontend side: in charge of the infrastructure, CI/CD, choice of the technology, etc.<br>
+                        Backend side: tracking down performance issues, performing load tests, writing new features, maintaining the infrastructure.<br>
+                        <!-- Main challenges: making React and d3 play together nicely.
+                            Displaying thousands and thousands of data points as an SVG.
+                            Keeping the database healthy while handling over 60M time series up to date. -->
+                    </p>
+                    <slot slot='tech-used'>
+                        <li>TypeScript</li>
+                        <li>ReactJS; Redux; d3.js; SASS; Jest+Enzyme; Cypress</li>
+                        <li>Parcel; Gitlab CI</li>
+                        <li>Python3; Cython; Rust</li>
+                        <li>Flask; Nginx</li>
+                        <li>SQLAlchemy; PostgreSQL, ElasticSearch</li>
+                        <li>Docker; AWS ECS, S3, RDS, CW, Lambda, SQS, etc.</li>
+                    </slot>
+                </ExpEntry>
+                <ExpEntry>
+                    <p slot="desc">
+                        Web application in clinics by medical doctors to manage their patients, visits, prescriptions, lab tests etc.
+                    <br>
+                        I was responsible for the client-facing API, calling other services via gRPC and exposing a GraphQL API.
+                        Our team was also responsible for the frontend and design.
+                    </p>
+                    <slot slot='tech-used'>
+                        <li>
+                            TypeScript
+                        </li>
+                        <li>
+                            NodeJS; GraphQL; gRPC; jest
+                        </li>
+                        <li>
+                            React; Apollo; styled-components; react-testing-library; Webpack
+                        </li>
+                        <li>Google Kubernetes Engine</li>
+                    </slot>
+                </ExpEntry>
+                <ExpEntry>
+                    <p slot="desc">
+                        Alexa integration for a smart oven and infrastructure optimizations for handling more users while being more cost effective. <br>
+                        I was responsible for developing new features for the integration, load testing, writing proposals for enhancements and implementing them.
+                    <slot name='tech-used'>
+                        <li>Python2</li>
+                        <li>Tornado; ELK Stack; Graphana</li>
+                        <li>AWS EC2, Lambda, DynamoDB, Kinesis, ElastiCache (Redis)</li>
+                        <li>MZBench; locust</li>
+                    </slot>
+                </ExpEntry>
+                <ExpEntry>
+                    <p slot="desc">
+                        An internal application for tracking leaves and project allocation.<br>
+                        Delivered features both on the frontend and the backend.
+                    </p>
+                    <slot name='tech-used'>
+                        <li>TypeScript</li>
+                        <li>ReactJS; Redux; jest+enzyme; SASS</li>
+                        <li>Python3</li>
+                        <li>Flask; SQLAlchemy; PostgreSQL</li>
+                        <li>AWS Beanstalk, RDS, S3, etc.</li>
+                    </slot>
+                </ExpEntry>
             </ul>
         </li>
     </ol>
@@ -44,12 +104,9 @@
         display: grid;
         gap: 1rem;
         grid-template:
-        "header strip content" auto
-        "bio strip content" 1fr
-        "bio strip content" 1fr / 380px 4px auto;
-    }
-    .strip {
-        grid-area: strip;
+        "header break content" auto
+        "bio break content" 1fr
+        "bio break content" 1fr / min(40vw, 25em) 0.5rem minmax(min-content, 40em);
     }
     main>header {
         grid-area: header;
@@ -62,5 +119,4 @@
     main>ol {
         grid-area: content;
     }
-
 </style>
---
+++ src/ExpEntry.svelte
@@ -0,0 +1,8 @@
+
+<li class='py-1' role="listitem">
+    <slot name="desc"></slot>
+    <p class='font-semibold'>Tech used:</p>
+    <ul class='list-disc list-inside'>
+        <slot name='tech-used'></slot>
+    </ul>
+</li>
