73b3f44c3ab2c2a47ce80702c356c930ab923516
jankiel <michal7410@gmail.com>
2020-11-05 14:05:04 +0100

    simplify the header

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,6 +1,6 @@
 <!-- <h3 class="text-2xl mt-4 text-gray-800 border-solid border-b-2 border-teal-600">Skills</h3> -->
 
 <h2 class="text-2xl text-gray-800 flex">
-  <span class="flex-grow-0 pl-2 sm:pl-0 border-l-2 sm:border-0 border-teal-600"><slot /></span>
-  <div class="hidden sm:block sm:ml-2 my-auto flex-grow border-b border-teal-600" />
+  <span class="flex-grow-0 border-teal-600"><slot /></span>
+  <div class="ml-2 my-auto flex-grow border-b border-teal-600" />
 </h2>
